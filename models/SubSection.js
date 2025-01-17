2907394d5f321a3e3a9af5da205ba4b8ff3c8c78
developernilesh <nileshmukherjee2017@gmail.com>
2025-01-17 23:39:05 +0530

    User, Profile, Section, SubSection & CourseProgress schemas are created

---
+++ models/SubSection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const subSectionSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  timeDuration: { type: String, required: true },
+  description: { type: String, required: true },
+  videoUrl: { type: String, required: true },
+});
+
+module.exports = mongoose.model("SubSection", subSectionSchema);
