d2f531a213a77fb98e736fb0f28b8990d763fd15
developernilesh <nileshmukherjee2017@gmail.com>
2025-01-31 23:58:36 +0530

    Tags controller continued

---
+++ controllers/Tags.js
@@ -0,0 +1,44 @@
+const Tag = require("../models/Tags");
+
+// creating new tag
+exports.createTag = async (req, res) => {
+  try {
+    // fetching data from req body
+    const { name, description } = req.body;
+
+    // validation for the inputs
+    if (!name || !description) {
+      res.status(401).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // creating entry in db
+    const tagDetails = await Tag.create({ name, description })
+
+    // success response
+    res.status(200).json({
+      success: true,
+      message: "Tag created successfully!"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+// fetching all tags
+exports.showAllTags = async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
