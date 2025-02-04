aca3e8795e5a8ed29f0fc9c14ca5c17c6faf37fc
developernilesh <nileshmukherjee2017@gmail.com>
2025-02-04 23:26:21 +0530

    Section Controller Completed

---
+++ controllers/Section.js
@@ -0,0 +1,106 @@
+const Course = require("../models/Course");
+const Section = require("../models/Section");
+
+// creating a new section
+exports.createSection = async (req, res) => {
+  try {
+    // fetching data from req body
+    const { sectionName, sectionId } = req.body
+
+    // validations
+    if (!sectionName) {
+      return res.status(401).json({
+        success: false,
+        message: "Please put the section name!"
+      })
+    }
+    if (!sectionId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unable to update section. Please try again!"
+      })
+    }
+
+    // creating entry in db for the new section
+    const newSection = await Section.create({ sectionName })
+
+    // adding the new section to the corresponding course
+    await Course.findByIdAndUpdate(sectionId, { $push: { courseContent: newSection._id } }, { new: true })
+
+    // success response
+    res.status(200).json({
+      success: true,
+      message: "Section Created Successfully!"
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong!"
+    })
+  }
+};
+
+// updating a section
+exports.updateSection = async (req, res) => {
+  try {
+    // fetching data from req body
+    const { sectionName, sectionId } = req.body
+
+    // validations
+    if (!sectionName) {
+      return res.status(401).json({
+        success: false,
+        message: "Please put the section name!"
+      })
+    }
+    if (!sectionId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unable to update the Section. Please try again!"
+      })
+    }
+
+    // adding the new section to the corresponding course
+    await Section.findByIdAndUpdate(sectionId, { sectionName }, { new: true })
+
+    // success response
+    res.status(200).json({
+      success: true,
+      message: "Section Updated Successfully!"
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong!"
+    })
+  }
+};
+
+exports.deleteSection = async (req, res) => {
+  try {
+    // fetching data from req body
+    const sectionId = req.body
+
+    // validations
+    if (!sectionId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unable to delete the section. Please try again!"
+      })
+    }
+
+    // deletion of the section
+    await Section.findByIdAndDelete(sectionId);
+
+    // success response
+    res.status(200).json({
+      success: true,
+      message: "Section deleted successfully!"
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong!"
+    })
+  }
+}
