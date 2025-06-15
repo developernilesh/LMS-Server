fed93dd7388e81f96b902cd6c1c42631f5188142
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-15 23:59:22 +0530

    Add ContactUs model schema for handling user inquiries

---
+++ models/ContactUs.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const contactUsSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  phoneNo: {
+    type: String,
+    trim: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("ContactUs", contactUsSchema);
